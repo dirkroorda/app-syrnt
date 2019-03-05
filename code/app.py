456abe4f50ebf9a3d3a8b421fc1dfd9278bd166e
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2019-03-05 10:54:57 +0100

    mit license

--- code/app.py
+++ code/app.py
@@ -152,6 +152,7 @@ class TfApp(object):
     otypeRank = api.otypeRank
     maxSlot = F.otype.maxSlot
     eoslots = E.oslots.data
+    isHtml = options.get('fmt', None) in app.textFormats
 
     bigType = False
     if nType in SECTION:
@@ -204,7 +205,8 @@ class TfApp(object):
       featurePart = ''
       occs = ''
       if nType == slotType:
-        text = htmlEsc(T.text([n], fmt=d.fmt))
+        text = T.text([n], fmt=d.fmt)
+        text = text if isHtml else htmlEsc(text)
         tClass = 'sy' if d.fmt is None or '-orig-' in d.fmt else 'tr'
         heading = f'<div class="{tClass}">{text}</div>'
         featurePart = getFeatures(
